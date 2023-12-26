ad9c309918986370dc52c91b452f40a594a71a1e
kadun1 <kadunjj@gmail.com>
2023-12-27 00:08:01 +0900

    spring_aop : cglib limit

--- src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
+++ src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -25,5 +25,18 @@ public class ProxyCastingTest {
         });
     }
 
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
 
+        //프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("proxy class={}", memberServiceProxy.getClass());
+
+        //CGLIB 동적 프록시를 구현 클래스로 캐스팅 시도 성공
+        MemberServiceImpl castingMemberServiceProxy = (MemberServiceImpl) memberServiceProxy;
+    }
 }
